78c9cb4d893bbf66def8e7c0f8ebdbe36093a28e
tesh254 <erik@renderheads.com>
2023-04-25 12:24:05 +0300

    add navbar items to navbar component

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,6 +3,18 @@ import React from "react";
 import Button from "../button";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
+import Link from "next/link";
+
+const links = [
+  {
+    label: "Dashboard",
+    link: "/",
+  },
+  {
+    label: "Billing",
+    link: "/billing",
+  },
+];
 
 const Navbar = () => {
   const supabaseClient = useSupabaseClient();
@@ -11,7 +23,20 @@ const Navbar = () => {
   return (
     <div className="w-full bg-white fixed top-0 h-[81px] z-30">
       <div className="max-w-screen-lg py-[20px] mx-auto w-full flex place-items-center justify-between">
-        <img className="w-[144px]" src="/logo-svg-1.svg" alt="gray-suportal-logo" />
+        <img
+          className="w-[144px]"
+          src="/logo-svg-1.svg"
+          alt="gray-suportal-logo"
+        />
+        <div className="flex space-x-[64px]">
+          {links.map((link) => {
+            return (
+              <Link className="hover:text-suportal-blue transition duration-300" href={link.link} key={link.link}>
+                {link.label}
+              </Link>
+            );
+          })}
+        </div>
         <Button
           onClick={() => {
             supabaseClient.auth.signOut();
