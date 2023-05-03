5c6b4e7e83b3b484ee030a7ef5cb0ecb311c94e6
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-03 21:36:46 +0100

    Update 11

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -45,8 +45,8 @@ export default function Layout({
       )}
       <div className="w-screen flex flex-row justify-between">
         <Navbar />
-        <main id="skip" className="w-full bg-white mt-[80px]">
-         <section className="max-w-screen-lg mx-auto px-2 py-[16px]">
+        <main id="skip" className="w-10/12 bg-white">
+         <section className="max-w-screen-lg mx-auto px-2 py-[16px] mt-[80px]">
            {children}
          </section>
         </main>
