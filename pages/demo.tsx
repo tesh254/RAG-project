ac297f13b1a3ac74d9674603f97a305e8ea4e990
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-05 21:47:55 +0100

    New Demo page

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -8,9 +8,13 @@ import { toast } from "react-hot-toast";
 import Counter from "../components/counter";
 import { protocol } from "../lib/sanitizer";
 import ApiKey from "../components/api-key";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import Button from "../components/button";
 
-
-return (
+const Demo: NextPage = () => {
+  const router = useRouter();
+    return (
     <Layout title="Suportal - Demo">
       <div className="w-[600px] p-[24px] bg-white rounded-[25px]">
         <h1 className="font-bold text-[22px]">Demo</h1>
@@ -18,3 +22,6 @@ return (
     </Layout>
   );
 };
+
+
+export default Demo;
