f5a389d3b305fbfd903947630993eede816c8ad9
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-05 21:51:56 +0100

    New Demo Page

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -1,19 +1,8 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Layout from "../components/layout";
-import { User } from "@supabase/supabase-js";
-import { GetServerSidePropsContext, NextPage } from "next";
-import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import axios from "axios";
-import { toast } from "react-hot-toast";
-import Counter from "../components/counter";
-import { protocol } from "../lib/sanitizer";
-import ApiKey from "../components/api-key";
 import { NextPage } from "next";
-import { useRouter } from "next/router";
-import Button from "../components/button";
 
 const Demo: NextPage = () => {
-  const router = useRouter();
     return (
     <Layout title="Suportal - Demo">
       <div className="w-[600px] p-[24px] bg-white rounded-[25px]">
@@ -22,6 +11,5 @@ const Demo: NextPage = () => {
     </Layout>
   );
 };
-
 
 export default Demo;
