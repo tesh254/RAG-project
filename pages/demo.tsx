bd488f4f5aae87b5f6adeff785f81c766496871e
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-06 09:13:26 +0100

    New Demo

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -7,6 +7,8 @@ const Demo: NextPage = () => {
     <Layout title="Suportal - Demo">
       <div className="w-[600px] p-[24px] bg-white rounded-[25px]">
         <h1 className="font-bold text-[22px]">Demo</h1>
+        <iframe src="https://app.suportal.co/widget/70697327-fe80-499e-86e7-97ff16616396" width="100%" height="300" style="border:1px solid black;">
+        </iframe>      
       </div>
     </Layout>
   );
