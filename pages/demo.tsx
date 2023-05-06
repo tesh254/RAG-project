d9d747ca73b5096f730e6ffd47a109915c397048
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-06 10:33:52 +0100

    testing

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -3,7 +3,12 @@ import Layout from "../components/layout";
 import { NextPage } from "next";
 import { User } from "@supabase/supabase-js";
 import { useUser } from "@supabase/auth-helpers-react";
-import {createClient} from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://lkxdecuqbkjplmgsrmrn.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxreGRlY3VxYmtqcGxtZ3NybXJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyMjM5MTUsImV4cCI6MTk5Njc5OTkxNX0.Yn3rjhVgvjilpHTOquiE-APYezroxWSFp2BZXLSOSLE';
+
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
 async function getUser() {
   const { user: userData, error: userError } = await supabaseClient.auth.api.getUser(req.headers.get('Authorization')!.replace('Bearer ', ''))
