74b8f4e737164f587d76c33cffc2097ee21b90c6
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-06 10:43:17 +0100

    fixing

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -3,16 +3,7 @@ import Layout from "../components/layout";
 import { NextPage } from "next";
 import { User } from "@supabase/supabase-js";
 import { useUser } from "@supabase/auth-helpers-react";
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lkxdecuqbkjplmgsrmrn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxreGRlY3VxYmtqcGxtZ3NybXJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyMjM5MTUsImV4cCI6MTk5Njc5OTkxNX0.Yn3rjhVgvjilpHTOquiE-APYezroxWSFp2BZXLSOSLE';
-
-const supabaseClient = createClient(supabaseUrl, supabaseKey);
-
-async function getUser() {
-  const { user: userData, error: userError }: any = await supabaseClient.auth.getUser(req.headers.get('Authorization')!.replace('Bearer ', ''))
-}
 
 const Demo: NextPage = () => {
     return (
@@ -21,7 +12,7 @@ const Demo: NextPage = () => {
         <h1 className="font-bold text-[22px]">Demo</h1>
         <iframe 
             className="w-full h-[600px] rounded-lg"
-            src="https://app.suportal.co/widget/">
+            src="https://app.suportal.co/widget/${user.id}">
         </iframe>      
       </div>
     </Layout>
