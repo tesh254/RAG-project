438b17f84161c99df6401fa0f2c94d75c22f769b
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-06 10:10:46 +0100

    Testing

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -4,23 +4,13 @@ import { NextPage } from "next";
 import { User } from "@supabase/supabase-js";
 import { useUser } from "@supabase/auth-helpers-react";
 
-import { createClient } from '@supabase/supabase-js';
-
-// Initialize Supabase client
-const supabase = createClient('https://lkxdecuqbkjplmgsrmrn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxreGRlY3VxYmtqcGxtZ3NybXJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyMjM5MTUsImV4cCI6MTk5Njc5OTkxNX0.Yn3rjhVgvjilpHTOquiE-APYezroxWSFp2BZXLSOSLE');
-
-// Retrieve user ID
-async function fetchUserId() {
-  const { user, error } = supabase.auth.currentUser();
-  if (error) {
-    console.error(error);
-    return;
-  }
-  const userId = user.id;
-  console.log(userId);
-}
-
-fetchUserId();
+  const script = (
+    <span>
+      {`<script`}
+      <br />
+      {`/${user.id}"></script>`}
+    </span>
+  );
 
 const Demo: NextPage = () => {
     return (
@@ -29,7 +19,7 @@ const Demo: NextPage = () => {
         <h1 className="font-bold text-[22px]">Demo</h1>
         <iframe 
             className="w-full h-[600px] rounded-lg"
-            src="https://app.suportal.co/widget/${userId}">
+            src="https://app.suportal.co/widget/">
         </iframe>      
       </div>
     </Layout>
