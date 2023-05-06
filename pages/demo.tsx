7a5c4329bc8de8f330f20dc7ef7bd48ac0cc15bf
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-06 09:16:53 +0100

    Updated Demo

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -2,12 +2,13 @@ import React, { useCallback, useEffect, useState } from "react";
 import Layout from "../components/layout";
 import { NextPage } from "next";
 
+
 const Demo: NextPage = () => {
     return (
     <Layout title="Suportal - Demo">
       <div className="w-[600px] p-[24px] bg-white rounded-[25px]">
         <h1 className="font-bold text-[22px]">Demo</h1>
-        <iframe src="https://app.suportal.co/widget/70697327-fe80-499e-86e7-97ff16616396" width="100%" height="300" style="border:1px solid black;">
+        <iframe src="https://app.suportal.co/widget/${user.id}" width="100%" height="300" style="border:1px solid black;">
         </iframe>      
       </div>
     </Layout>
