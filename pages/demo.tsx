2d8d577769108cf7387e64a88134b55f4218df6c
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-06 09:40:00 +0100

    Fixing Demo script

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -3,6 +3,15 @@ import Layout from "../components/layout";
 import { NextPage } from "next";
 import { User } from "@supabase/supabase-js";
 
+const script = (
+    <span>
+      {`<script`}
+      <br />
+      {`src="${apiUrl}/widget`}
+      <br />
+      {`/${user.id}"></script>`}
+    </span>
+  );
 
 const Demo: NextPage = () => {
     return (
@@ -11,7 +20,7 @@ const Demo: NextPage = () => {
         <h1 className="font-bold text-[22px]">Demo</h1>
         <iframe 
             className="w-full h-[600px] rounded-lg"
-            src="https://app.suportal.co/widget/${user.id}">
+            src="https://app.suportal.co/widget/{script}">
         </iframe>      
       </div>
     </Layout>
