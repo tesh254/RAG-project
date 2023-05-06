e2c6bf54f3c710d665988547e49439df31a157a7
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-06 10:25:50 +0100

    testing

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -5,12 +5,6 @@ import { User } from "@supabase/supabase-js";
 import { useUser } from "@supabase/auth-helpers-react";
 import {createClient} from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-export const supabase = createClient (supabaseUrl, supabaseAnonKey)
-
 const Demo: NextPage = () => {
     return (
     <Layout title="Suportal - Demo">
