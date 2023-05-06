564cc8188ba4ae716811ce6e567273cd91101465
Adam Petty <77830549+adampetty@users.noreply.github.com>
2023-05-06 10:00:41 +0100

    Update demo 5

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -4,6 +4,12 @@ import { NextPage } from "next";
 import { User } from "@supabase/supabase-js";
 import { useUser } from "@supabase/auth-helpers-react";
 
+import { createClient } from '@supabase/supabase-js';
+
+// Initialize Supabase client
+const supabase = createClient('https://lkxdecuqbkjplmgsrmrn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxreGRlY3VxYmtqcGxtZ3NybXJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyMjM5MTUsImV4cCI6MTk5Njc5OTkxNX0.Yn3rjhVgvjilpHTOquiE-APYezroxWSFp2BZXLSOSLE');
+
+// Retrieve user ID
 const user = supabase.auth.user();
 const userId = user.id;
 console.log(userId);
